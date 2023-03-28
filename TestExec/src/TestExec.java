356e9fd98f64c6ad5d7a3698cc38648486636889
SergeyZhabinsky <szhab@list.ru>
2023-03-28 04:55:44 +0300

    28_03_2023
    Исправлен вывод русского языка на английский.

---
+++ TestExec/src/TestExec.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class TestExec {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        String s;
+        Valid v = new Valid();
+        while (true){
+//            System.out.println("\n\nВведите символ 'Q' для выхода");
+            System.out.println("\n\nEnter character 'Q' to exit");
+            s = scan.nextLine();
+
+            if(s.toUpperCase().equals("Q")) break;
+
+            if(v.start(s))
+                System.out.println("Answer: " + v.getResult());
+//                System.out.println("Ответ: " + v.getResult());
+
+        }
+    }
+//=====================================================================
+
+
+
+    public static String calc(String input){
+        String result = "";
+        return result;
+    }
+}
